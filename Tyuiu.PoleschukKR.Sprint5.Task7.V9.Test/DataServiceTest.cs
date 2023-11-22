609dfd1025efbf5f4fbde7061b247a071c8e6ca6
Tyuiu.PoleschukKR@outlook.com <kirillpolesuk05@gmail.com>
2023-11-22 09:36:27 +0500

    Task7

---
+++ Tyuiu.PoleschukKR.Sprint5.Task7.V9.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PoleschukKR.Sprint5.Task7.V9.Lib;
+using System.IO;
+
+
+namespace Tyuiu.PoleschukKR.Sprint5.Task7.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckedExistsFile()
+        {
+            string path = $@"C:\DataSprint5\InPutDataFileTask7V9.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
